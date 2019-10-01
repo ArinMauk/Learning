8c2372120a7d5286ab0da1841715e26ef7d8bd56
Arin <arin.mauk7@gmail.com>
2019-09-30 19:40:21 -0500

    Hello World of Learning

---
+++ c++/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// Command line, go to directory of the file.
+// Compiling: g++ <fileName>
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
