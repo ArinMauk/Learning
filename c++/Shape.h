c0496db126afdd04a6c4999fc032db402d4c3863
Arin <arin.mauk7@gmail.com>
2019-10-01 22:07:28 -0500

    Added Notes About Virtual Functions and Derived classes.

--- c++/Shape.h
+++ c++/Shape.h
@@ -1,6 +1,10 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 
+// NOTE: For virtual methods, you must include some degree of implmentation. 
+// eg: virtual double getArea(); doesn't work; you cant override that in derived classes.
+// virtual double getArea(){return 0.0;}; does work because it has some default implementation.
+
 class Shape{
     public: 
         virtual double getArea(){return 0.0;};
